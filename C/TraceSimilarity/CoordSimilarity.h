ab176f43958ec48effcc219966107e6d319d0dbe
calvin <calvinneo1995@gmail.com>
2016-05-12 00:36:48 +0800

    update results

--- C/TraceSimilarity/CoordSimilarity.h
+++ C/TraceSimilarity/CoordSimilarity.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include "def.h"
+#include <vector>
+
+struct CoordSimilarity {
+
+};
+
+CoordSimilarity CoordCompare(std::vector<Point> t1, std::vector<Point> t2);
+CoordSimilarity CoordList(std::vector<Point> t1, std::vector< std::vector<Point> > tlist);
--- C/TraceSimilarity/TimeSimilarity.h
+++ C/TraceSimilarity/TimeSimilarity.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include "def.h"
+#include <vector>
+
+struct TimeSimilarity {
+
+};
+
+TimeSimilarity TimeCompare(std::vector<TPoint> t1, std::vector<TPoint> t2);
+TimeSimilarity TimeList(std::vector<TPoint> t1, std::vector< std::vector<TPoint> > tlist);
