5fd6c24dca21a2ebaef6368ff81dbc74defd6465
calvin <calvinneo1995@gmail.com>
2016-05-14 15:00:20 +0800

    bugfix

--- C/JavaSimulator/main.cpp
+++ C/JavaSimulator/main.cpp
@@ -11,6 +11,71 @@ using namespace std;
 #define IP "127.0.0.1"
 
 int wmain(int argc, TCHAR* argv[], TCHAR* env[]) {
+	WSADATA wsaData;
+	//init Winsock.dll
+	if (WSAStartup(MAKEWORD(1, 1), &wsaData) != 0)
+	{
+		printf("fail to init winsock.dll!\n");
+		exit(-1);
+	}
+	//creat socket
+	SOCKET servSoc;
+	//SOCK_STREAM is TCP
+	if ((servSoc = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
+	{
+		printf("create socket error!\n");
+		exit(-1);
+	}
+
+	struct sockaddr_in servAddr;
+	memset(&servAddr, 0, sizeof(sockaddr_in));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_port = htons(PORT);
+	servAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+
+	// bind addr
+	if (bind(servSoc, (sockaddr*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+	{
+		printf("bind error! port:%d\n", PORT);
+		exit(-1);
+	}
+	// max connection = 2
+	if (listen(servSoc, 2) == SOCKET_ERROR)
+	{
+		printf("listen error!\n");
+		exit(-1);
+	}
+	printf("Server %d is listening...\n", PORT);
+	SOCKET clientSoc;
+	struct sockaddr_in clientAddr;
+	memset(&clientAddr, 0, sizeof(clientAddr));
+	int addrlen = sizeof(clientAddr);
+	//accept
+	if ((clientSoc = accept(servSoc, (sockaddr*)&clientAddr, &addrlen)) == INVALID_SOCKET)
+	{
+		printf("accept error!\n");
+		exit(-1);
+	}
+	printf("Accept connection from %s\n", inet_ntoa(clientAddr.sin_addr));
+	char buf[4096];
+	while (1)
+	{
+		int bytes;
+		if ((bytes = recv(clientSoc, buf, sizeof(buf), 0)) == SOCKET_ERROR)
+		{
+			printf("recv error!\n");
+			exit(-1);
+		}
+		buf[bytes] = '\0';
+		printf("Message from %s:%s\n", inet_ntoa(clientAddr.sin_addr), buf);
+		if (send(clientSoc, buf, bytes, 0) == SOCKET_ERROR)
+		{
+			printf("send error!\n");
+			exit(-1);
+		}
+	}
+	//clean socket
+	WSACleanup();
 	system("pause");
 	return 0;
 }
--- C/TraceSimilarity/main.cpp
+++ C/TraceSimilarity/main.cpp
@@ -19,6 +19,37 @@ using namespace std;
 
 vector<string> csvname;
 
+
+std::string ws2s(const std::wstring& ws)
+{
+	std::string curLocale = setlocale(LC_ALL, NULL);        // curLocale = "C";
+	setlocale(LC_ALL, "chs");
+	const wchar_t* _Source = ws.c_str();
+	size_t _Dsize = 2 * ws.size() + 1;
+	char *_Dest = new char[_Dsize];
+	memset(_Dest, 0, _Dsize);
+	wcstombs(_Dest, _Source, _Dsize);
+	std::string result = _Dest;
+	delete[] _Dest;
+	setlocale(LC_ALL, curLocale.c_str());
+	return result;
+}
+
+std::wstring s2ws(const std::string& s)
+{
+	setlocale(LC_ALL, "chs");
+	const char* _Source = s.c_str();
+	size_t _Dsize = s.size() + 1;
+	wchar_t *_Dest = new wchar_t[_Dsize];
+	wmemset(_Dest, 0, _Dsize);
+	mbstowcs(_Dest, _Source, _Dsize);
+	std::wstring result = _Dest;
+	delete[] _Dest;
+	setlocale(LC_ALL, "C");
+	return result;
+}
+
+
 vector<Point> read_csv(wstring path) {
 	int record_count = 0;
 	HANDLE hfile = CreateFile(path.c_str(), GENERIC_READ, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_READONLY, NULL);
