84a3b07d16a58f05c2edffebb63d41fea63a4eaa
calvin <calvinneo1995@gmail.com>
2016-05-14 14:43:19 +0800

    socket

---
+++ C/JavaSimulator/main.cpp
@@ -0,0 +1,16 @@
+#include <windows.h>
+#include <string>
+#include <time.h>
+#include <vector>
+
+using namespace std;
+
+#define UNICODE
+
+#define PORT 15777
+#define IP "127.0.0.1"
+
+int wmain(int argc, TCHAR* argv[], TCHAR* env[]) {
+	system("pause");
+	return 0;
+}
